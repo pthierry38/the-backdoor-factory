3fda2500e26c46b324ad81fd02a9cc62470331a4
Philippe Thierry <phil@reseau-libre.net>
2017-05-30 16:39:43 +0200

    add uscan conf to build orig tarball without incompatible DFSG content

--- debian/orig-tar.sh
+++ debian/orig-tar.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -e
 
+set -x
 # called by uscan with '--upstream-version' <version> <file>
 PKG=backdoor-factory
 DIR=$PKG-$2
@@ -7,6 +8,9 @@ TAR=../${PKG}_$2+dfsg.orig.tar.gz
 
 # clean up the upstream tarball
 tar xvf $3
+# renaming
+mv the-backdoor-factory-$2 backdoor-factory-$2
+
 tar -c -z -f $TAR -X debian/orig-tar.exclude $DIR
 rm -rf $DIR $3
 
